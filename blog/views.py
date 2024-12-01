6ac6b6c389ba62fae392ef3298d94c77702c0919
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 12:56:06 +0330

    add newsletter form to view

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Category, Tag
+from django.shortcuts import render, get_object_or_404, redirect
+from blog.models import Post, Category, Tag, User
+from blog.forms import NewsletterForm
+from django.contrib import messages
 
 
 # Create your views here.
 def index(request):
     category = request.GET.get("category")
     tag = request.GET.get("tag")
+    author = request.GET.get("author")
 
     posts = Post.objects.filter(status="published")
+
+    if author:
+        try:
+            author = User.objects.get(username=author)
+            posts = posts.filter(author=author)
+        except User.DoesNotExist:
+            posts = Post.objects.none()
 
     if category:
         try:
@@ -22,6 +32,24 @@ def index(request):
             posts = posts.filter(tags=tag)
         except Tag.DoesNotExist:
             posts = Post.objects.none()
+
+    if request.method == "POST":
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Thank you for subscribing to our newsletter!")
+            return redirect("blog:index")
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field.capitalize()}: {error}")
+
+    return render(request, "index.html", context={"posts": posts})
+
+
+def blog_search(request):
+    search_query = request.GET.get("q", "")
+    posts = Post.objects.filter(status="published", content__icontains=search_query)
 
     return render(request, "index.html", context={"posts": posts})
 
