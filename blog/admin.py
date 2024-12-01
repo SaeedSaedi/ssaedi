39c3ce4aef2d02dce6563a92f83d7c02386bc908
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 17:29:02 +0330

    add summernote for content

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import Category, Tag, Post, Newsletter
+from django_summernote.admin import SummernoteModelAdmin
 
 
 # Register your models here.
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ("title", "author", "category", "status", "created_at", "updated_at")
     list_filter = ("status", "category", "tags")
     search_fields = ("title", "content")
+    summernote_fields = ("content",)
 
 
 admin.site.register(Category)
