58959aa6d9d7dca9895286b086b5bb87fd207453
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 10:01:12 +0330

    add url for single post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 # urls.py
 from django.urls import path
-from blog.views import index, contact
+from blog.views import index, contact, post_single
 
 app_name = "blog"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("posts/<int:post_id>/", post_single, name="post_detail"),
     path("contact-us", contact, name="contact"),
 ]
