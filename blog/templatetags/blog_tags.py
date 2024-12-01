b34998398d6854a910a1000cd0fcd2c46c5655cc
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 15:23:15 +0330

    create param replace function to replace page param instead of creating new

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -49,3 +49,13 @@ def get_category_counts():
         category_counts[category.name] = post_count
 
     return category_counts
+
+
+@register.simple_tag(takes_context=True)
+def param_replace(context, **kwargs):
+    d = context["request"].GET.copy()
+    for k, v in kwargs.items():
+        d[k] = v
+    for k in [k for k, v in d.items() if not v]:
+        del d[k]
+    return d.urlencode()
