0a29f63b06a5193b1facd9007a942fd33f4d616e
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 17:29:15 +0330

    create form for add new post

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from blog.models import Newsletter
+from django_summernote.widgets import SummernoteWidget
+from blog.models import Newsletter, Post
 
 
 class NewsletterForm(forms.ModelForm):
@@ -17,3 +18,34 @@ class NewsletterForm(forms.ModelForm):
         if Newsletter.objects.filter(email=email).exists():
             raise forms.ValidationError("This email is already subscribed.")
         return email
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "content",
+            "category",
+            "image",
+            "thumbnail",
+            "tags",
+            "status",
+        ]
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "content": SummernoteWidget(attrs={"class": "form-control"}),
+            "category": forms.Select(attrs={"class": "form-control"}),
+            "image": forms.ClearableFileInput(attrs={"class": "form-control"}),
+            "thumbnail": forms.ClearableFileInput(attrs={"class": "form-control"}),
+            "tags": forms.SelectMultiple(attrs={"class": "form-control"}),
+            "status": forms.Select(attrs={"class": "form-control"}),
+        }
+
+    def save(self, commit=True, user=None):
+        instance = super().save(commit=False)
+        if user:
+            instance.author = user
+        if commit:
+            instance.save()
+        return instance
