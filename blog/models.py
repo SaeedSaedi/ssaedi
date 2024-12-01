aec51e0f3a59db2cc024fc496581f4f6c6ce5c6c
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 10:00:52 +0330

    change the image file names while upload

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,29 @@
+import os
+from uuid import uuid4
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
+from django.utils.deconstruct import deconstructible
+from django.conf import settings
 
 
 # Create your models here.
+@deconstructible
+class UploadToPathAndRename(object):
+
+    def __init__(self, path):
+        self.sub_path = path
+
+    def __call__(self, instance, filename):
+        ext = filename.split(".")[-1]
+        if instance.pk:
+            filename = "{}.{}".format(instance.pk, ext)
+        else:
+            filename = "{}.{}".format(uuid4().hex, ext)
+        return os.path.join(self.sub_path, filename)
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
@@ -33,7 +52,13 @@ class Post(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True, blank=True
     )
-    image = models.ImageField(upload_to="blog/", default="blog/default.jpg")
+    image = models.ImageField(
+        upload_to=UploadToPathAndRename("posts/"), default="posts/default.jpg"
+    )
+    thumbnail = models.ImageField(
+        upload_to=UploadToPathAndRename("posts/thumbnails/"),
+        default="posts/thumbnails/default.jpg",
+    )
     tags = models.ManyToManyField(Tag, blank=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="draft")
     created_at = models.DateTimeField(default=timezone.now)
