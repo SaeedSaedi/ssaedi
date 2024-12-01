d190cbf147255fe9872d3a8ed76a7f5e74155e1e
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 10:00:27 +0330

    register post in admin with a postadmin model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Category, Tag, Post
 
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "category", "status", "created_at", "updated_at")
+    list_filter = ("status", "category", "tags")
+    search_fields = ("title", "content")
+
+
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
