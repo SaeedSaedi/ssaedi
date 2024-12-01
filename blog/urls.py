b57c99f0b0a222c4a8843d3234a2ec7cda20b244
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 12:55:50 +0330

    add url for search form

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 # urls.py
 from django.urls import path
-from blog.views import index, contact, post_single
+from blog.views import index, contact, post_single, blog_search
 
 app_name = "blog"
 
 urlpatterns = [
     path("", index, name="index"),
     path("posts/<int:post_id>/", post_single, name="post_detail"),
+    path("search/", blog_search, name="search"),
     path("contact-us", contact, name="contact"),
 ]
