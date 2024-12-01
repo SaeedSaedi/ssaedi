f45b5050acbf4bb386a2b61aba35c9da4f9ee062
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 14:53:29 +0330

    script for paginator

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -6,3 +6,17 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 3000);
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const paginatorLinks = document.querySelectorAll('.paginator .page-link');
+    paginatorLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const url = this.href;
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+            setTimeout(() => {
+                window.location.href = url;
+            }, 500);
+        });
+    });
+});
