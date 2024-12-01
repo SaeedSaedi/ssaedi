e5b935dcd5b2ca4fa07ad67a85ac758beba3dad1
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 17:29:24 +0330

    add summernote url path

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 # urls.py
 from django.urls import path
-from blog.views import index, contact, post_single, blog_search
+from blog.views import index, contact, post_single, blog_search, create_post
 
 app_name = "blog"
 
 urlpatterns = [
     path("", index, name="index"),
     path("posts/<int:post_id>/", post_single, name="post_detail"),
+    path("posts/create_new/", create_post, name="create_new_post"),
     path("search/", blog_search, name="search"),
     path("contact-us", contact, name="contact"),
 ]
