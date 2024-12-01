600ac2559fc784464eca2f763a1d9f2d1625b6fe
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 10:02:02 +0330

    add view for single post

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    posts = Post.objects.filter(status="Published")
+
+    return render(request, "index.html", context={"posts": posts})
 
 
 def contact(request):
     return render(request, "contactus.html")
+
+
+def post_single(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status="Published")
+
+    post.counted_views += 1
+    post.save()
+
+    context = {
+        "post": post,
+    }
+
+    return render(
+        request,
+        "blog-single.html",
+        context=context,
+    )
