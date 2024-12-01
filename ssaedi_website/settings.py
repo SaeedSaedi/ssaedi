f0d1b65f98a8bdd8615717b2464ac11b94c6a738
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-01 17:29:52 +0330

    add summernote to install app

--- ssaedi_website/settings.py
+++ ssaedi_website/settings.py
@@ -41,8 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
-    'django.contrib.sites',
+    "django.contrib.sites",
     "blog.apps.BlogConfig",
+    "django_summernote",
 ]
 
 SITE_ID = 2
@@ -143,3 +144,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
