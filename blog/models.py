2666de9215656c199a6db6e2dfd27a2023ef40a5
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-02 13:55:02 +0330

    create comment and contact models

--- blog/models.py
+++ blog/models.py
@@ -80,3 +80,25 @@ class Newsletter(models.Model):
 
     def __str__(self):
         return self.email
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
+
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Contact from {self.first_name} on {self.created_date}"
