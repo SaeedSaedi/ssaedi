2aacb6dfe62834f92e5f24bff5c7c51c6d5d38f3
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-02 16:54:43 +0330

    update url

--- ssaedi_website/urls.py
+++ ssaedi_website/urls.py
@@ -43,5 +43,5 @@ urlpatterns = [
     path('feed/', LatestPostsFeed(), name='latest_posts_feed'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
