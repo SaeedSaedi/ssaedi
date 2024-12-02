4b608ddea459d3283fac05fa450043310d769d26
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-02 12:46:36 +0330

    create user form for profile page

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -11,3 +12,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username")
