c52552f47979f911923715e2d4ed8635a199b604
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-02 13:12:23 +0330

    add custom form for auth

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,7 +3,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import UserProfile
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -17,4 +18,28 @@ class CustomUserCreationForm(UserCreationForm):
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ("first_name", "last_name", "username")
+        fields = ("first_name", "last_name", "username", "email")
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label="Username or Email")
+
+    def clean(self):
+        username_or_email = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if username_or_email and password:
+            if "@" in username_or_email:
+                try:
+                    user = User.objects.get(email=username_or_email)
+                    username = user.username
+                except User.DoesNotExist:
+                    raise forms.ValidationError("Invalid email or password.")
+            else:
+                username = username_or_email
+
+            self.user_cache = authenticate(username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError("Invalid username or password.")
+
+        return self.cleaned_data
