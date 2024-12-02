d2cdfc51c31cb2dc42a35f1114de2e4000ad677d
saeed saedi <saeed.saedi.s@gmail.com>
2024-12-02 11:44:17 +0330

    add accounts url to main

--- ssaedi_website/urls.py
+++ ssaedi_website/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path("accounts/", include("accounts.urls")),
 ]
 
 if settings.DEBUG:
